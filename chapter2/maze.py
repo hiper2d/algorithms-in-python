2290d4aeecaf789f5d2f81868f5a6367d4bb73ee
Alexey Zelenovsky <hiper2d@gmail.com>
2020-11-16 20:40:02 -0500

    Chapter 2.2 Added toString to Maze

--- chapter2/maze.py
+++ chapter2/maze.py
@@ -4,7 +4,7 @@ from typing import NamedTuple, List
 
 
 class Cell(str, Enum):
-    EMPTY = ' '
+    EMPTY = '.'
     BLOCKED = 'X'
     PATH = '*'
     START = 'S'
@@ -18,8 +18,8 @@ class MazeLocation(NamedTuple):
 
 class Maze:
     def __init__(self,
-                 rows: int = 9,
-                 columns: int = 9,
+                 rows: int = 10,
+                 columns: int = 10,
                  sparseness: float = .2,
                  start: MazeLocation = MazeLocation(0, 0),
                  goal: MazeLocation = MazeLocation(9, 9),
@@ -31,10 +31,22 @@ class Maze:
         self._grid: List[List[Cell]] = [[Cell.EMPTY for c in range(columns)] for r in range(rows)]
         self._randomly_fill(rows, columns, sparseness)
         self._grid[start.row][start.column] = Cell.START
-        self._grid[goal.row][goal.column] = Cell.START
+        self._grid[goal.row][goal.column] = Cell.GOAL
+
+    def __str__(self) -> str:
+        output: str = ""
+        for row in self._grid:
+            output += ''.join([c.value for c in row]) + '\n'
+        return output
 
     def _randomly_fill(self, rows: int, columns: int, sparseness: float) -> None:
         for r in range(rows):
             for c in range(columns):
                 if random.uniform(0, 1.0) <= sparseness:
                     self._grid[r][c] = Cell.BLOCKED
+
+
+if __name__ == "__main__":
+    maze: Maze = Maze()
+    print(maze)
+
