9adca9d8e295aa4361840c36efc8b0e523b49745
Alexey Zelenovsky <hiper2d@gmail.com>
2021-02-20 18:18:09 -0500

    Chapter 2.3 Added successors method to calculate all possible next states for particular state

--- chapter2/missionaries.py
+++ chapter2/missionaries.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import List
+
 MAX_NUM: int = 3
 
 
@@ -8,6 +11,43 @@ class MCState:
         self.em: int = MAX_NUM - self.wm
         self.ec: int = MAX_NUM - self.wc
         self.boat = boat
+        
+    @property
+    def is_legal(self) -> bool:
+        if 0 < self.wm < self.wc:
+            return False
+        if 0 < self.em < self.ec:
+            return False
+        return True
+        
+    def goal_test(self):
+        return self.is_legal and self.em == MAX_NUM and self.ec == MAX_NUM
+    
+    def successors(self) -> List[MCState]:
+        sucs: List[MCState] = []
+        if self.boat:
+            if self.wm > 1:
+                sucs.append(MCState(self.wc, self.wm - 2, not self.boat))
+            if self.wm > 0:
+                sucs.append(MCState(self.wc, self.wm - 1, not self.boat))
+            if self.wc > 1:
+                sucs.append(MCState(self.wc - 2, self.wm, not self.boat))
+            if self.wc > 0:
+                sucs.append(MCState(self.wc - 1, self.wm, not self.boat))
+            if self.wc > 0 and self.wm > 0:
+                sucs.append(MCState(self.wc - 1, self.wm - 1, not self.boat))
+        else:
+            if self.em > 1:
+                sucs.append(MCState(self.ec, self.em + 2, not self.boat))
+            if self.em > 0:
+                sucs.append(MCState(self.ec, self.em + 1, not self.boat))
+            if self.ec > 1:
+                sucs.append(MCState(self.ec + 2, self.em, not self.boat))
+            if self.ec > 0:
+                sucs.append(MCState(self.ec + 1, self.em, not self.boat))
+            if self.ec > 0 and self.em > 0:
+                sucs.append(MCState(self.ec + 1, self.em + 1, not self.boat))
+        return [x for x in sucs if x.is_legal]
 
     def __str__(self) -> str:
         return ("West bank has {} cannibals and {} missionaries.\n"
