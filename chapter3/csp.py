4bd26870c3fae8a7b8161da5d7f773fe929d7213
hiper2d <hiper2d@gmail.com>
2021-05-16 21:58:11 -0400

    Added some methods to CSP

--- chapter3/csp.py
+++ chapter3/csp.py
@@ -13,3 +13,27 @@ class Constraint(Generic[V, D], ABC):
     @staticmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         ...
+
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]):
+        self.variables: List[V] = variables
+        self.domains: Dict[V, List[V]] = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in variables:
+            self.constraints[variable] = []
+            if variable not in domains:
+                raise LookupError("Every variable should have domain assigned to it")
+
+    def add_constraint(self, constraint: Constraint[V, D]):
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("Variable in constraint not in CSP")
+            else:
+                self.constraints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: List[V, D]) -> bool:
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
