9e82c47da95a89d3e54c362114cb9762dcfcd22e
hiper2d <hiper2d@gmail.com>
2021-12-30 20:55:34 -0500

    7.1 Neural Network: Added sigmoid activation function

--- chapter7_neural_network/util.py
+++ chapter7_neural_network/util.py
@@ -1,5 +1,11 @@
+from math import exp
 from typing import List
 
 
 def dot_product(sx: List[float], ys: List[float]) -> float:
     return sum([x * y for x, y in zip(sx, ys)])
+
+
+# the classic sigmoid activation function
+def sigmoid(x: float) -> float:
+    return 1.0 / (1.0 + exp(-x))
