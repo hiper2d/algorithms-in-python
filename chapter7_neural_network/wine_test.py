e1e66ffb1e5386fe9b1dcf41f0289d1c1b4eec15
hiper2d <hiper2d@gmail.com>
2022-03-17 19:35:28 -0400

    7.5.3 Finished wine classification via Neural Network

--- chapter7_neural_network/wine_test.py
+++ chapter7_neural_network/wine_test.py
@@ -2,14 +2,25 @@ import csv
 from random import shuffle
 from typing import List
 
+from chapter7_neural_network.network import Network
 from chapter7_neural_network.util import normalize_by_feature_scaling
+
+
+def wine_interpret_output(output: List[float]) -> int:
+    if max(output) == output[0]:
+        return 1
+    elif max(output) == output[1]:
+        return 2
+    else:
+        return 3
+
 
 if __name__ == "__main__":
     wine_parameters: List[List[float]] = []
     wine_classifications: List[List[float]] = []
     wine_species: List[int] = []
     with open('wine.data', 'r') as wine_data:
-        wines: List = list(csv.reader(wine_data, quting=csv.QUOTE_NONNUMERIC))
+        wines: List = list(csv.reader(wine_data, quoting=csv.QUOTE_NONNUMERIC))
         shuffle(wines)
         for wine in wines:
             parameters: List[float] = [float(n) for n in wine[1:14]]
@@ -23,4 +34,19 @@ if __name__ == "__main__":
                 wine_classifications.append([0.0, 0.0, 1.0])
             wine_species.append(species)
     normalize_by_feature_scaling(wine_parameters)
+    wine_network: Network = Network([13, 7, 3], 0.9)
+
+    # train over the first 150 wines 10 times
+    wine_trainers: List[List[float]] = wine_parameters[:150]
+    wine_trainers_corrects: List[List[float]] = wine_classifications[:150]
+    for _ in range(10):
+        wine_network.train(wine_trainers, wine_trainers_corrects)
+
+    # test over the last 28 of wines in the data set
+    wine_testers: List[List[float]] = wine_parameters[150:]
+    wine_testers_corrects: List[int] = wine_species[150:]
+    wine_results = wine_network.validate(wine_testers,
+                                         wine_testers_corrects, wine_interpret_output)
+    print(f"{wine_results[0]} correct of {wine_results[1]} = {wine_results[2] * 100}%")
+
 
