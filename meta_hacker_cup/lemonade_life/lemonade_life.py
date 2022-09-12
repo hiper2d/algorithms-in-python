37ccc9f0bade446444645e24c2b08095f456d69e
hiper2d <hiper2d@gmail.com>
2022-09-12 18:43:18 -0400

    Implemented a better Convex Hull with n*log(n)

--- meta_hacker_cup/lemonade_life/lemonade_life.py
+++ meta_hacker_cup/lemonade_life/lemonade_life.py
@@ -1,6 +1,7 @@
 import math
 import sys
 from dataclasses import dataclass
+from functools import cmp_to_key
 from typing import List, Set
 
 
@@ -8,9 +9,6 @@ from typing import List, Set
 class House:
     x: int
     y: int
-    is_convex: bool
-    is_start: bool
-    is_end: bool
     damage: int = -1
     visited: bool = False
 
@@ -26,7 +24,8 @@ class Testcase:
     n: int
     k: int
     d: int
-    houses: List[House]
+    hull: List[House]
+    target: House
 
 
 def read_int(file):
@@ -50,13 +49,10 @@ class Solution:
                 houses: List[House] = []
                 for i in range(n):
                     [x, y] = read_array_of_str(f)
-                    is_convex = True if i == 0 or i == n-1 else False
-                    is_start = True if i == 0 else False
-                    is_end = True if i == n - 1 else False
-                    houses.append(House(int(x), int(y), is_convex, is_start, is_end))
+                    houses.append(House(int(x), int(y)))
 
-                testcase = Testcase(n, k, d, houses)
-                self.detect_convex_hull(testcase)
+                hull = self.detect_convex_hull(houses)
+                testcase = Testcase(n, k, d, hull, houses[-1])
                 testcases.append(testcase)
         return testcases
 
@@ -69,11 +65,11 @@ class Solution:
 
     @staticmethod
     def process_testcase(t: Testcase) -> int:
-        convex_houses = [h for h in t.houses if h.is_convex and not h.is_start]
-        start = t.houses[0]
+        convex_houses = t.hull
+        start = t.hull[0]
         start.damage = 0
         start.visited = True
-        target = t.houses[-1]
+        target = t.target
 
         def find_next(starting_house: House) -> List[House]:
             next_houses: List[House] = []
@@ -101,27 +97,45 @@ class Solution:
         return target.damage
 
     @staticmethod
-    def detect_convex_hull(t: Testcase):
+    def detect_convex_hull(houses: List[House]) -> List[House]:
         """
         This method is super slow (n^2) and cannot handle the 'lemonade_life_input.txt' file
         Try this one (n*log(n)): https://www.geeksforgeeks.org/convex-hull-set-2-graham-scan/
         """
-        start = 0
-        p = start
-        while True:
-            t.houses[p].is_convex = True
-            q = (p + 1) % t.n
-            for i in range(t.n):
-                orientation = Solution.orientation(t.houses[p], t.houses[i], t.houses[q])
-                if orientation == 2:
-                    q = i
-                elif orientation == 0:
-                    # skip collinear, keep only furthest as convex
-                    if Solution.furthest(t.houses[p], t.houses[i], t.houses[q]) == 1:
-                        q = i
-            p = q
-            if p == start:
-                break
+        p0 = houses[0]
+        n = len(houses)
+
+        def compare(p1: House, p2: House):
+            o = Solution.vertical_orientation(p0, p1, p2)
+            if o == 0:
+                if Solution.sqr_dist(p0, p1) > Solution.sqr_dist(p0, p1):
+                    return -1
+                else:
+                    return 1
+            else:
+                if o == 2:
+                    return -1
+                else:
+                    return 1
+
+        points = sorted(houses, key=cmp_to_key(compare))
+        m = 1
+        for i in range(1, n):
+            while (i < n - 1) and Solution.vertical_orientation(p0, points[i], points[i+1]) == 0:
+                i += 1
+            points[m] = points[i]
+            m += 1
+
+        if m < 3:
+            return [p0, houses[-1]]
+
+        hull = []
+        hull.extend(points[:3])
+        for i in range(3, m):
+            while len(hull) > 0 and Solution.vertical_orientation(hull[-2], hull[-1], points[i]) != 2:
+                hull.pop()
+            hull.append(points[i])
+        return hull
 
     @staticmethod
     def orientation(p: House, q: House, r: House):
@@ -134,6 +148,25 @@ class Solution:
         """
         val = (q.y - p.y) * (r.x - q.x) - \
               (q.x - p.x) * (r.y - q.y)
+
+        if val == 0:
+            return 0
+        elif val > 0:
+            return 1
+        else:
+            return 2
+
+    @staticmethod
+    def vertical_orientation(p: House, q: House, r: House):
+        """
+        To find orientation of ordered triplet (p, q, r).
+        The function returns following values
+        0 --> p, q and r are collinear
+        1 --> Clockwise
+        2 --> Counterclockwise
+        """
+        val = (q.x - p.x) * (r.y - q.y) - \
+              (q.y - p.y) * (r.x - q.x)
 
         if val == 0:
             return 0
@@ -168,6 +201,6 @@ if __name__ == "__main__":
     #     for line in f:
     #         count += len(line)
     # print(count)
-
-    #print(Solution.process_testcase(solution.testcases[0]))
+    # print(solution.testcases[2].hull)
+    # print(Solution.process_testcase(solution.testcases[2]))
 
